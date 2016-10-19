e4e14bc59614717ad0df155add3d0eb48f869b54
nz3.manu <nz3.manu@gmail.com>
2016-10-19 08:34:24 -0500

    Jekyll-workflow-with-Gulp: gulp-cssnano, gulp-postcss, gulp-sourcemaps, gulp-uglify, precss, autoprefixer

--- assets/javascripts/js.js
+++ assets/javascripts/js.js
@@ -1,4 +1,5 @@
 var hola = function () {
-  console.log("Hola mundo");
+  console.log('Hola mundo');
+  console.log('inserte un numero:');
 }
 hola()
--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var cp          = require('child_process');
-var plumber = require('gulp-plumber');
+var browserSync = require('browser-sync');
+var cp = require('child_process');
+//var plumber = require('gulp-plumber');
 var postcss = require('gulp-postcss');
 var uglify = require('gulp-uglify');
 var cssnano = require('gulp-cssnano');
@@ -9,36 +9,65 @@ var autoprefixer = require('autoprefixer');
 var precss = require('precss');
 var sourcemaps = require('gulp-sourcemaps');
 
-gulp.task('css', function () {
-  var processors = [
-    autoprefixer({browsers: ['last 1 version']}),precss()
-];
-  return gulp.src('assets/stylesheets/main.css')
-    .pipe(sourcemaps.init())
-    .pipe(plumber())
-    .pipe(postcss(processors))
-    .pipe(cssnano())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
+
+// ---------- Jekyll
+var jekyll   = process.platform === 'win32' ? 'jekyll.bat' : 'jekyll';
+var messages = {
+    jekyllBuild: '<span style="color: grey">Running:</span> $ jekyll build'
+};
+//-- Build the Jekyll Site
+gulp.task('jekyll-build', function (done) {
+    browserSync.notify(messages.jekyllBuild);
+    return cp.spawn( jekyll , ['build'], {stdio: 'inherit'})
+        .on('close', done);
+});
+//-- Rebuild Jekyll & do page reload
+gulp.task('jekyll-rebuild', ['jekyll-build'], function () {
+    browserSync.reload();
+});
+//-- Wait for jekyll-build, then launch the Server
+gulp.task('browser-sync', ['css', 'compress', 'jekyll-build'], function() {
+    browserSync({
+        server: {
+            baseDir: '_site'
+        },
+        notify: false
+    });
 });
 
-gulp.task('reload',browserSync.reload);
+// ----  Final Jekyll
+
+gulp.task('css', function() {
+    var processors = [
+        autoprefixer({
+            browsers: ['last 1 version']
+        }), precss()
+    ];
+    return gulp.src('assets/css/main.css')
+        .pipe(sourcemaps.init())
+        //.pipe(plumber())
+        .pipe(postcss(processors))
+        .pipe(cssnano())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./_site/css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('reload', browserSync.reload);
 gulp.task('compress', function() {
-  return gulp.src('assets/javascripts/*.js')
-    .pipe(sourcemaps.init())
-    .pipe(plumber())
-    .pipe(uglify())
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest('./javaScript'))
-    .pipe(browserSync.stream());
+    return gulp.src('assets/javascripts/**/*.js')
+        .pipe(sourcemaps.init())
+        //.pipe(plumber())
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./_site/javascript'))
+        .pipe(browserSync.stream());
 });
 
-gulp.task('default', function() {
-  browserSync.init({
-    server: "./"
+gulp.task('watch', function() {
+    gulp.watch('assets/css/**', ['css']);
+    gulp.watch('assets/javascripts/**/*.js', ['compress', 'reload']);
+    gulp.watch(['*.html', '_layouts/*.html', '_posts/*'], ['jekyll-rebuild']);
 });
-  gulp.watch('assets/stylesheets/*.css', ['css']);
-  gulp.watch('assets/javascripts/*.js', ['compress', 'reload']);
-  gulp.watch("index.html").on('change', browserSync.reload);
-});
+
+gulp.task('default', ['browser-sync', 'watch']);
